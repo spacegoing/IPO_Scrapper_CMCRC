8f26e9967f3aaff019cf7466ca48c3b068128a40
spacegoing <spacegoing@gmail.com>
2019-04-05 13:05:30 +1100

    [New] ASX Database

--- ipo/pipelines.py
+++ ipo/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+mkt_db = client['IPO']
 
 
 class IpoPipeline(object):
-    def process_item(self, item, spider):
-        return item
+
+  def process_item(self, item, spider):
+    col_name = item['uptick_name']
+    if item['error']:
+      mkt_db[col_name + '_error_urls'].insert_one(item)
+    else:
+      mkt_db[col_name].insert_one(item)
+    return item
+
+  def close_spider(self, spider):
+    client.close()
+
+
