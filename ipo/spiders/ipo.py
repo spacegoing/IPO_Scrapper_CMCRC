d7fa249e20494c71222e51ee4d85b4328ee7d4e4
spacegoing <spacegoing@gmail.com>
2019-04-05 13:11:45 +1100

    [New] Finished Basic Spider

--- ipo/spiders/ipo.py
+++ ipo/spiders/ipo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import dateparser as dp
+import traceback
 
 
 class LoginSpider(scrapy.Spider):
@@ -13,24 +14,33 @@ class LoginSpider(scrapy.Spider):
     yield scrapy.Request(self.website_url, callback=self.parse)
 
   def parse(self, response):
-    from scrapy.shell import inspect_response
-    inspect_response(response, self)
+    # from scrapy.shell import inspect_response
+    # inspect_response(response, self)
     row_list = response.xpath('//table//tr[td/@class="name"]')
     for r in row_list:
-      name = r.xpath('./td[1]/text()').extract_first().strip()
-      ric = r.xpath('string(./td[2])').extract_first().strip()
-      date_str = r.xpath('string(./td[3])').extract_first().strip()
-      date = dp.parse(
-          date_str,
-          date_formats=['%d/%m/%Y'],
-          settings={
-              'TIMEZONE': self.tzinfo,
-              'RETURN_AS_TIMEZONE_AWARE': True
-          })
-      yield {
-          "name": name,
-          "ric": ric,
-          "date": date,
-          "tzinfo": self.tzinfo,
-          "uptick_name": self.uptick_name
-      }
+      try:
+        name = r.xpath('./td[1]/text()').extract_first().strip()
+        ric = r.xpath('string(./td[2])').extract_first().strip()
+        date_str = r.xpath('string(./td[3])').extract_first().strip()
+        date = dp.parse(
+            date_str,
+            date_formats=['%d/%m/%Y'],
+            settings={
+                'TIMEZONE': self.tzinfo,
+                'RETURN_AS_TIMEZONE_AWARE': True
+            })
+        yield {
+            "name": name,
+            "ric": ric,
+            "date": date,
+            "tzinfo": self.tzinfo,
+            "uptick_name": self.uptick_name,
+            "error": False
+        }
+      except Exception as e:  # not news row, skip
+        yield {
+            'error': True,
+            'row_html': r.extract(),
+            'error_message': '%s: %s' % (e.__class__, str(e)),
+            'traceback': traceback.format_exc(),
+        }
